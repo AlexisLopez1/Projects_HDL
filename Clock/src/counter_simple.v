e4061813215d9e5f1e7a593e890937d4ad5d7323
Alexis <alexis.sbtg@gmail.com>
2021-11-07 22:57:41 -0600

    Clock completed using IFs.

--- Clock/src/counter_simple.v
+++ Clock/src/counter_simple.v
@@ -6,18 +6,71 @@ module counter_simple
 	Enable,
 	Clk,
 	Clr,
-	Q
+	Q,
+	Q1,
+	Q2,
+	Q3,
+	Q4,
+	Q5
 );
 
 	input Enable  ;
 	input Clk, Clr ;
-	output reg [WIDTH-1:0] Q;
+	output reg [WIDTH-1:0] Q, Q1, Q2, Q3, Q4, Q5;
+	reg [WIDTH-1:0] i;
 
 	always @(negedge Clr, posedge Clk)
 	begin
-		if (Clr == 1'b0) 
-			Q <= 0;		
-		else if (Enable) 
+		if (Clr == 1'b0) begin
+			Q <= 0;	
+		end
+//		else if (Q == 4'b1010) begin
+//			Q <= 0;
+//		end
+		else if (Enable) begin
 			Q <= Q + 1;
+			if(Q == 4'b1001) begin
+				Q <= 0;
+				Q1 <= Q1 + 1;
+				
+				if(Q1 == 4'b0101) begin
+					Q1 <= 0;
+					Q2 <= Q2 + 1;
+					
+					if(Q2 == 4'b1001) begin
+						Q2 <= 0;
+						Q3 <= Q3 + 1;
+						
+						if(Q3 == 4'b0101) begin
+							Q3 <= 0;
+							Q4 <= Q4 + 1;
+							
+							if(Q4 == 4'b1001) begin
+								Q4 <= 0;
+								Q5 <= Q5 + 1;
+								
+								if(Q5 == 4'b0101) begin
+									Q5 <= 0;
+								end
+							end
+						end
+					end
+				end
+					
+			end
+		end
 	end
+	
+//		always @(negedge Clr, Q, posedge Clk)
+//	begin
+//		if (Clr == 1'b0) begin
+//			Q1 <= 0;	
+//		end
+//		else if (Q == 3'b101) begin
+//			Q1 <= 0;
+//		end
+//		else if (Enable) begin
+//			Q1 <= Q1 + 1;
+//		end
+//	end
 endmodule		
--- Clock/src/counter_wrapper_1s_RCO.v
+++ Clock/src/counter_wrapper_1s_RCO.v
@@ -13,26 +13,26 @@ module counter_wrapper_1s_RCO (MAX10_CLK1_50, SW, HEX5, HEX4, HEX3, HEX2, HEX1, 
 	//////////// LED //////////
 	output [9:0]		LEDR;
 	
-	parameter WIDTH = 12;
+	parameter WIDTH = 4;
 	
 	wire Clock = MAX10_CLK1_50 ;
 	wire Resetn = SW[0];
 	wire Enable;
 	// 4-bit counter based on T-flip flops
-	wire [WIDTH-1:0] Count;
+	wire [WIDTH-1:0] Count, Count1, Count2, Count3, Count4, Count5;
 
 	// Counter Instantiation
-	 counter_simple # (.WIDTH(WIDTH)) counter (.Enable(Enable), .Clk(Clock), .Clr(Resetn), .Q(Count));
+	 counter_simple # (.WIDTH(WIDTH)) counter (.Enable(Enable), .Clk(Clock), .Clr(Resetn), .Q(Count), .Q1(Count1), .Q2(Count2), .Q3(Count3), .Q4(Count4), .Q5(Count5));
 	
 	cont_1s_RCO Clk_1s_RCO ( .mclk(MAX10_CLK1_50), .reset(Resetn), .RCO(Enable) );  // RCO generation for Enable other counters
 
 	// drive the displays
-	hex7seg digit0 (Count[1:0], HEX0);
-	hex7seg digit1 (Count[3:2], HEX1);
-	hex7seg digit2 (Count[5:4], HEX2);
-	hex7seg digit3 (Count[7:6], HEX3);
-	hex7seg digit4 (Count[9:8], HEX4);
-	hex7seg digit5 (Count[11:10], HEX5);
+	hex7seg digit0 (Count[3:0], HEX0);
+	hex7seg digit1 (Count1[3:0], HEX1);
+	hex7seg digit2 (Count2[3:0], HEX2);
+	hex7seg digit3 (Count3[3:0], HEX3);
+	hex7seg digit4 (Count4[3:0], HEX4);
+	hex7seg digit5 (Count5[3:0], HEX5);
 	// drive the LEDs
 	assign LEDR ={Count,Enable,SW};  //Display Count, Enable (RCO) and Reset 
 endmodule
